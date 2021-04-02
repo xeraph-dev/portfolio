ef55d8b7af4353a18348ee518a1671eee74b8876
adrianrl99 <61700204+adrianrl99@users.noreply.github.com>
2021-04-02 00:28:17 -0400

    update favicon and logo, and reorganized appbar buttons

--- src/components/header/appbar/HeaderAppBar.tsx
+++ src/components/header/appbar/HeaderAppBar.tsx
@@ -1,4 +1,5 @@
-import { AppBar, Box, Container, Toolbar } from '@material-ui/core'
+import { AppBar, Box, Button, Container, Toolbar } from '@material-ui/core'
+import Image from 'next/image'
 import CVButton from './CVButton'
 import GitHubButton from './GitHubButton'
 import PageTitle from './PageTitle'
@@ -9,11 +10,16 @@ export default function HeaderAppBar(): JSX.Element {
     <AppBar color="default" position="fixed">
       <Container>
         <Toolbar variant="dense">
-          <PageTitle />
-          <Box flexGrow={1} />
+          <Button disabled>
+            <Image src="/marca.png" height={35} width={158} />
+          </Button>
           <CVButton />
+          <Box flexGrow={1} />
           <ToggleTheme />
           <GitHubButton />
+          <Button disabled>
+            <PageTitle />
+          </Button>
         </Toolbar>
       </Container>
     </AppBar>
