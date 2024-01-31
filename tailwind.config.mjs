0dee2d95668c367c07cd7b4f008c8bfe377f1a63
Xeraph <61700204+xeraph-dev@users.noreply.github.com>
2024-01-31 17:30:59 -0500

    [site] some fixes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,7 @@
 import { addDynamicIconSelectors } from '@iconify/tailwind'
 import forms from '@tailwindcss/forms'
 import typography from '@tailwindcss/typography'
+import plugin from 'tailwindcss/plugin'
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -11,7 +12,27 @@ export default {
 			fontFamily: {
 				nunito: ['Nunito', 'sans-serif'],
 			},
+
+			textShadow: {
+				sm: '0 1px 2px var(--tw-shadow-color)',
+				DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+				lg: '0 8px 16px var(--tw-shadow-color)',
+			},
 		},
 	},
-	plugins: [forms({ strategy: 'class' }), typography(), addDynamicIconSelectors()],
+	plugins: [
+		forms({ strategy: 'class' }),
+		typography(),
+		addDynamicIconSelectors(),
+		plugin(function ({ matchUtilities, theme }) {
+			matchUtilities(
+				{
+					'text-shadow': value => ({
+						textShadow: value,
+					}),
+				},
+				{ values: theme('textShadow') },
+			)
+		}),
+	],
 }
