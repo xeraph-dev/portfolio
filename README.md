7092b8e58926feab393a72f7c577171ed5fe0c10
Xeraph <61700204+xeraph-dev@users.noreply.github.com>
2024-02-03 14:41:51 -0500

    [site] add sitemap

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # My personal portfolio
+
+- Favicon generated in [favicon.io](https://favicon.io/favicon-generator)
+  - text: `{x}`
+  - background: `rounded`
+  - font family: `Amita`
+  - font variant: `bold`
+  - font size: `90`
+  - font color: `#F8FAFC`
+  - background color: `#020617`
--- astro.config.ts
+++ astro.config.ts
@@ -2,12 +2,24 @@ import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 import { DEFAULT_LOCALE, LOCALES } from './i18n/i18n'
 
+import sitemap from '@astrojs/sitemap'
+
+const site = 'https://xeraph.dev'
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://xeraph.dev',
+	site,
 	i18n: {
 		defaultLocale: DEFAULT_LOCALE,
 		locales: LOCALES,
 	},
-	integrations: [tailwind()],
+	integrations: [
+		tailwind(),
+		sitemap({
+			i18n: {
+				defaultLocale: DEFAULT_LOCALE,
+				locales: LOCALES.reduce((acc, code) => ({ ...acc, [code]: code }), {}),
+			},
+		}),
+	],
 })
