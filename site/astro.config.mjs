7f4eff4b0d19c2757a29bf65a6a63d1baab7e268
Xeraph <61700204+xeraph-dev@users.noreply.github.com>
2024-05-04 11:38:57 -0400

    fix project

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,15 +1,22 @@
-import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
-import sitemap from "@astrojs/sitemap";
-import robotsTxt from "astro-robots-txt";
-import mdx from "@astrojs/mdx";
-import svelte from "@astrojs/svelte";
 import astroI18next from "astro-i18next";
+import robotsTxt from "astro-robots-txt";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://xeraph.dev",
+  vite: {
+    build: {
+      rollupOptions: {
+        external: ["shiki/themes/hc_light.json"]
+      }
+    }
+  },
   integrations: [
     tailwind(),
     compress(),
